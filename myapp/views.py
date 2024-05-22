2bb5c6c807e4a53e2b772d645c50c121d8448833
Nico98Ar <rakcin785@gmail.com>
2024-05-22 15:30:39 -0500

    cambio3

--- myapp/views.py
+++ myapp/views.py
@@ -1,11 +1,24 @@
 
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from .models import Project , Task
 # Create your views here.
 
+def index(request):
+    return HttpResponse("HOLA MUNDOOOOO")
 
-def hello(request):
-    return HttpResponse("<h1>HOLAAAAAAAAA</h1>")
+
+def hello(request, username):
+    print(username)
+    return HttpResponse("<h1>HOLAAAAAAAAA %s</h1>" % username)
 
 def about(request):
-    return HttpResponse("fhgdftjfdj")
+    return HttpResponse("NICOOOOOOOOOOOOO")
+
+def projects(request):
+    projects = list(Project.objects.values())
+    return JsonResponse(projects, safe=False)
+    
+def task(request):
+    task =list(Task.objects.values())
+    return JsonResponse(task, safe=False)
     
