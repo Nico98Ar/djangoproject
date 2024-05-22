7033ac258fb12b9ae5eef806a55ac29136128430
Nico98Ar <rakcin785@gmail.com>
2024-05-22 09:10:37 -0500

    cambio2

--- myapp/models.py
+++ myapp/models.py
@@ -4,3 +4,8 @@ from django.db import models
 
 class Project(models.Model):
     name =models.CharField(max_length=200)
+
+class Task(models.Model):
+    title= models.CharField(max_length=200)
+    descripcion= models.TextField()
+    project = models.ForeignKey(Project,  on_delete=models.CASCADE)
