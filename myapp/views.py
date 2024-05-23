7f447475ff80f701821640e20c65e9733cbf66ac
Nico98Ar <rakcin785@gmail.com>
2024-05-23 09:07:35 -0500

    htmls

--- myapp/views.py
+++ myapp/views.py
@@ -1,25 +1,29 @@
 
 from django.http import HttpResponse, JsonResponse
 from .models import Project , Task
+from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return HttpResponse("HOLA MUNDOOOOO")
+    return render(request, 'index.html')
 
+def about(request):
+    return render(request, "about.html")
 
 def hello(request, username):
     print(username)
     
     return HttpResponse("<h1>HOLAAAAAAAAA %s</h1>" % username)
 
-def about(request):
-    return HttpResponse("NICOOOOOOOOOOOOO")
 
 def projects(request):
-    projects = list(Project.objects.values())
-    return JsonResponse(projects, safe=False)
+   # projects = list(Project.objects.values())
+    projects = list(Project.objects.all())
+    return render(request, 'projects.html',{
+        'projects': projects
+    })
     
 def task(request):
-    task =list(Task.objects.values())
-    return JsonResponse(task, safe=False)
+    #task =list(Task.objects.values())
+    return render(request, 'task.html')
     
